e6591c360c9d6a48d092679deec9008ab23fb626
MRROBOT\nicai <nicaiczy1996@gmail.com>
2017-06-09 22:54:07 +0800

    first_trial

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
+from api_key import api_key
 
 veid ='293750'
-api_key =
+base_url = 'https://api.64clouds.com/v1/getServiceInfo?'
+
+params = {
+    'veid': veid,
+    'api_key': api_key,
+}
+
+r = requests.get(base_url, params=params)
+
+print(type(r.json()))
